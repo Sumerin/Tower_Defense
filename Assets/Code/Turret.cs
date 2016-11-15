34d1637b7354efdcce2c32c338ac7b6ab7836f00
Stanisław Góra <Stanisaw Gra>
2016-11-15 19:53:32 +0100

    Basic Shooting

--- Assets/Code/Turret.cs
+++ Assets/Code/Turret.cs
@@ -3,49 +3,64 @@ using System.Collections;
 
 public class Turret : MonoBehaviour {
 
+	public GameObject bulletPrefab;
 	Transform turret;
 	Monster_AI enemy;
+
 	float findEnemyIdleTime = 0.5f, findEnemyTimer = 0;
-	float range = 150;
+	float shootCooldown = 1f, shootCooldownTimer = 0;
+	public float range = 150;
 
 	// Use this for initialization
-	void Start () {
-		turret = transform.Find ("Turret");
+	void Start() {
+		turret = transform.Find("Turret");
 		findEnemy();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		if(enemy == null) {
+	void Update() {
+		if (enemy == null) {
 			findEnemyTimer += Time.deltaTime;
-			if(findEnemyTimer >= findEnemyIdleTime) {
+			if (findEnemyTimer >= findEnemyIdleTime) {
 				findEnemyTimer = 0;
-				findEnemy ();
+				findEnemy();
 			}
 		}
-		if(enemy != null) {
+		if (enemy != null) {
 			float dist = Vector3.Distance(this.transform.position, enemy.transform.position);
 			if (dist <= range) {
 				Quaternion dir = Quaternion.LookRotation(enemy.transform.position - this.transform.position);
-				turret.rotation = Quaternion.Euler (0, dir.eulerAngles.y + 90, 0);
+				turret.localRotation = Quaternion.Euler(0, dir.eulerAngles.y, 0);
+				shootCooldownTimer += Time.deltaTime;
+				if (shootCooldownTimer >= shootCooldown) {
+					shootCooldownTimer = 0;
+					shoot();
+				}
 			} else
 				enemy = null;
 		}
+	}
+
+	void shoot() {
+		GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+		bulletGO.transform.SetParent(this.transform);
+		Bullet bullet = bulletGO.GetComponent<Bullet>();
+		bullet.target = enemy.transform;
 	}
 
 	void findEnemy() {
 		Monster_AI[] monsters = FindObjectsOfType<Monster_AI>();
 		int nearest = -1;
 		float distance = Mathf.Infinity;
-		for(int i = 0; i < monsters.Length; i++) {
+		for (int i = 0; i < monsters.Length; i++) {
 			float thisDist = Vector3.Distance(this.transform.position, monsters[i].transform.position);
-			if(thisDist < distance && thisDist <= range) {
+			if (thisDist < distance && thisDist <= range) {
 				nearest = i;
 				distance = thisDist;
 			}
 		}
-		if(nearest >= 0) {
-			enemy = monsters [nearest];
+		if (nearest >= 0) {
+			enemy = monsters[nearest];
 		}
 	}
 
